4ae455c44697a6c9399c9daa10fe73651f662619
gd-harco <gd-harco@student.42lyon.fr>
2024-02-04 18:38:24 +0100

    Start Work on new message handeling. server now ust read from fd, send the read result to the client "handleString" function.
    HandleString is not currently implemented
    
    Took 1 minute

--- srcs/client.cpp
+++ srcs/client.cpp
@@ -1,4 +1,5 @@
 #include "client.hpp"
+#include "utility.hpp"
 
 Client::Client()
 {
@@ -57,6 +58,33 @@ void Client::addMessageToSendbox(std::string message) {
 	}
 	this->_msgToSend.push(message);
 }
+
+//TODO: take this back here
+void	Client::handleString(const std::string &toParse) {
+	this->_clientBuffer.append(toParse);
+	std::queue<std::string> toProcess = split(this->_clientBuffer, DELIMITER);
+	while (!toProcess.empty()) {
+		if (toProcess.front().find(DELIMITER) == std::string::npos) {
+			this->_clientBuffer = toProcess.front();
+			return;
+		}
+
+	}
+	// {
+	// 	if (HandleCommand(msg, _clients[fd]) == true && _clients[fd]->GetPassword() == true && !_clients[fd]->GetNickname().empty() && !_clients[fd]->GetUsername().empty())
+	// 	{
+	// 		for (fdClientMap::iterator cur = _clients.begin(); cur != _clients.end(); ++cur){
+	// 			if (cur->first == fd)
+	// 				continue;
+	// 			cur->second->addMessageToSendbox(msg);
+	// 			cur->second->updateClientStatus(this->_epollFd);
+	// 		}
+	//
+	// 	}
+	// 	msg.clear();
+	// }
+}
+
 
 void Client::receiveMsg() {
 	std::string toSend = _msgToSend.front();
