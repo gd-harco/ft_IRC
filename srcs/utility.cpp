070fb82d10136cbbb9516d4c48ce8d8c3420b75e
gd-harco <gd-harco@student.42lyon.fr>
2024-02-04 18:22:41 +0100

    split now keep the delimiter at the end of the message to handle potential non delimiter-terminated reception
    Took 5 minutes
    
    Took 8 minutes

--- srcs/utility.cpp
+++ srcs/utility.cpp
@@ -1,11 +1,13 @@
 #include "utility.hpp"
 
+#include <client.hpp>
+
 std::queue<std::string> split(const std::string &toSplit, const std::string &delimiter) {
 	std::queue<std::string> returned;
 	unsigned long start = 0;
 	unsigned long end = toSplit.find(delimiter);
 	while (end != std::string::npos) {
-		returned.push(toSplit.substr(start, end - start));
+		returned.push(toSplit.substr(start, end - start) + delimiter);
 		start = end + delimiter.size();
 		end = toSplit.find(delimiter, start);
 	}
