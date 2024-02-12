21f45c4e0d2d0c234374819180aecd66485e2b55
gd-harco <gd-harco@student.42lyon.fr>
2024-02-12 15:05:31 +0100

    Added security in case of client trying to connect with a nickname already use. Server keep a set of all nickname used. When a client is removed, so is his nickname
    Took 3 minutes

--- srcs/commands/Nick.cpp
+++ srcs/commands/Nick.cpp
@@ -6,6 +6,14 @@
 
 void Server::nick(vectorCommand args, Client *client)
 {
+	if (this->_nickUsed.find(args[1]) != this->_nickUsed.end()) {
+		NumericReplies::Error::nickInUse(*client, args[1]);
+		client->updateClientStatus(this->_epollFd);
+		throw NickAlreadyExist();
+	}
+	if (!client->GetNickname().empty())
+		this->_nickUsed.erase(client->GetNickname());
+	this->_nickUsed.insert(args[1]);
 	client->SetNickname(args[1]);
 	std::cout << client->GetNickname() << " " << client->GetUsername() << " " << client->GetFd() << ": " << "have new nickname" << std::endl;
 }
