14bb1c9fc5d760d8217c693e33b37f7f14c0a1d4
zorkz <alexandre.stachnik2@gmail.com>
2024-02-26 17:33:22 +0100

    fix user

--- srcs/commands/User.cpp
+++ srcs/commands/User.cpp
@@ -6,15 +6,25 @@
 
 void Server::user(vectorCommand args, Client *client)
 {
-	// for (fdClientMap::iterator it = _clients.begin(); it != _clients.end(); it++)
-	// {}
+	if (args.size() < 5)
+	{
+		NumericReplies::Error::needMoreParams(*client, "PASS");
+		client->updateClientStatus(_epollFd);
+		throw(NeedMoreParams());
+	}
 	if (client->IsAuthenticate()){
 		NumericReplies::Error::alreadyRegistered(*client);
 		client->updateClientStatus(_epollFd);
 		throw AlreadyRegistred();
 	}
-	std::string	RealName = args[args.size() - 1].substr(1, args[args.size() - 1].find("\r") - 1);
-	client->SetRealname(RealName);
-	client->SetUsername(args[1]);
-	std::cout << client->GetNickname() << " " << client->GetUsername() << " " << client->GetFd() << ": " << "have new username" << std::endl;
+	if (args[args.size() - 1].find("\r") != args[args.size() - 1].npos)
+	{
+		std::string	RealName = args[args.size() - 1].substr(1, args[args.size() - 1].find("\r") - 1);
+		client->SetRealname(RealName);
+		client->SetUsername(args[1]);
+		std::cout << client->GetNickname() << " " << client->GetUsername() << " " << client->GetFd() << ": " << "have new username" << std::endl;
+	}
+	NumericReplies::Error::needMoreParams(*client, "PASS");
+	client->updateClientStatus(_epollFd);
+	throw(NeedMoreParams());
 }
