f51a687e4e85927d9492d85c9af18e4f442ccf2b
gd-harco <gd-harco@student.42lyon.fr>
2024-02-26 15:32:21 +0100

    removed leftover's TODO

--- srcs/commands/Quit.cpp
+++ srcs/commands/Quit.cpp
@@ -6,7 +6,6 @@
 
 void Server::quit(vectorCommand args, Client *client)
 {
-	//TODO Mettre la bonne NR
 	client->_toDestroy = true;
 	NumericReplies::Notification::error(*client, "Quit :" + args[1]);
 	client->updateClientStatus(this->_epollFd);
